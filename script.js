8f80904e1c4ae014f9ee687678c2528db3733c5d
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-03-03 13:01:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,18 +7,24 @@ class Person {
 	getName(){
 		return this.name;
 	}
-	setAge(age){
+	setAge(age) {
 		this.age = age;
 	}
 }
 
 class Student extends Person {
+	constructor(name, age) {
+        super(name, age);
+    }
 	study(){
 		console.log(`${this.name} is studying`);
 	}
 }
 
 class Teacher extends Person {
+	constructor(name, age) {
+        super(name, age);
+    }
 	teach(){
 		console.log(`${this.name} is teaching`)
 	}
